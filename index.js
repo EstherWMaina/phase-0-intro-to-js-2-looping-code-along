30126904319799785bc6af43f408ca570ba81e9d
Esther Wanjiku Maina <emaina@DESKTOP-GIBC6BL.localdomain>
2022-08-29 03:51:13 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 // Code your solutions in this file
-const array = [];
-
-function writeCards(names, event) {
-     
-    for (let i = 0; i < names.length; i++) {
-        array.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
-        debugger;
-            }
-    return array;
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+    debugger;
+  }
+  function writeCards(names, event) {
+	let message = [];
+	for (let i = 0; i < names.length; i++) {
+		message.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+	}
+	return message;
 }
-
-
-function countDown(i) {
-    while (i >= 0) {
-        console.log(i);
-        i--;
-    }
-}
+console.logwriteCards(["Ada", "Brendan", "Ali"], "birthday");
+function countDown(number) {
+	while (number >= 0) {
+		console.log(number);
+		number--;
+	}
+} 
